994e0019593a07723f1a57f45c7cd538b24d7955
amattock <134568545+amattock@users.noreply.github.com>
2023-09-04 13:04:55 -0500

    changed made to server.js

--- server.js
+++ server.js
@@ -1,22 +1,69 @@
-// Dependencies
+//Requireing dependencies
 const express = require("express");
+//uuid is the unique identifier for creating the unique id.  
+const uuid = require("uuid").v4;
+//Used for handling and transforming file paths. 
+const path = require("path");
+//Logger allows dev to see traffic coming and going. 
+const logger = require("morgan");
+//responsible for all asynchronous and synchronous file operations.  
 const fs = require("fs");
+//Uses callback funtions (req, res) and renders html elements based on passing arguments. 
+const app = express();
 
+const PORT = process.env.PORT || 8080;
+var dataNotes = [];
 
-// Sets up the express app
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Sets up the express app to handle data parsing
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/public/assets", express.static(__dirname + "/public/assets"));
+app.use(express.static("public"));
 
+app.use(logger("dev"));
 
-require("./routes/htmlRoutes")(app);
-require("./routes/apiRoutes.js")(app);
-
-// Starts the server to begin listening
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+// API Routes - gets the file from index.js and reads it with fs.readfile.
+app.get("/api/notes", function(req, res) {
+  fs.readFile("db/db.json", "utf8", function(err, data) {
+    //Concatenates dataNotes to the array.
+    dataNotes = [].concat(JSON.parse(data));
+    res.json(JSON.parse(data));
+  });
 });
+// Returns the API notes and pushes to to the newNote array.
+app.post("/api/notes", function(req, res) {
+  const newNote = { id: uuid(), ...req.body };
+  dataNotes.push(newNote);
+  fs.writeFile("db/db.json", JSON.stringify(dataNotes), function(
+    //call back function for when data is loaded .
+    err,
+    data
+  ) {
+    console.log(err, data);
+    //sends the response to newNote
+    res.send(newNote);
+  });
+});
+//function that returns a response of "note not found" if dataNotes does not contain a note.
+app.delete("/api/notes/:id", function(req, res) {
+  //if dataNotes
+  var note = dataNotes.find(i => i.id === req.params.id);
+  if (!note) return res.send("note not found");
+  var index = dataNotes.indexOf(note);
+  dataNotes.splice(index, 1);
+  fs.writeFile("db/db.json", JSON.stringify(dataNotes), function(err, data) {
+    console.log(err, data);
+    res.send(true);
+  });
+});
+
+// HTML Routes
+app.get("/notes", function(req, res) {
+  //function requests a response to get a string from notes.
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+//
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+//listens for an assigned port otherwise port 8080 defined at the top.
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
